94d885b31f6a70f74cd917ee9c55a9b508972e19
Suraj Kumar <kumarsuraj.islampur@gmail.com>
2024-12-05 01:03:55 +0530

    added label of counting items in cart and modified some alignment

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,14 +4,18 @@ import { IoIosSearch } from "react-icons/io";
 import { Input } from "@/components/ui/input";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { selectCartItemCount } from "@/redux/cartSelector/cartSelector";
+
+import axios from "axios";
 
 const Header = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
+  const [loading, setLoading] = useState(false);
   const dropdownRef = useRef(null);
   const navigate = useNavigate();
   const userData = useSelector((state) => state?.meData);
-
+  const cartItemCount = useSelector(selectCartItemCount);
   const isLoggedIn = Boolean(userData);
 
   const handleLogin = () => {
@@ -19,9 +23,25 @@ const Header = () => {
     navigate("/signin");
   };
 
-  const handleLogout = () => {
+  const handleLogout = async (e) => {
     closeDropdown();
-    console.log("User logged out");
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await axios.post(
+        'http://localhost:3737/api/user/signout',
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      navigate("/home");
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   const toggleDropdown = useCallback(() => {
@@ -40,6 +60,7 @@ const Header = () => {
 
   useEffect(() => {
     const handleClickOutside = (e) => {
+      // Close the dropdown if the user clicks outside of it
       if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
         closeDropdown();
       }
@@ -55,9 +76,9 @@ const Header = () => {
   }, [dropdownOpen, closeDropdown]);
 
   return (
-    <div className="bg-silver flex items-center justify-center">
-      <header className="fixed top-0 left-0 right-0 bg-[rgba(0,0,0,0.4)] w-full p-4 z-50">
-        <div className="container mx-auto flex justify-between items-center">
+    <div className="bg-silver flex items-center justify-center ">
+      <header className="fixed top-0 left-0 right-0 bg-[rgba(0,0,0,0.4)] w-full p-4 z-50 backdrop-blur-sm ">
+        <div className="container mx-auto flex justify-between items-center ">
           {/* Navigation Links */}
           <div className="flex space-x-6 pl-7">
             <button
@@ -98,22 +119,30 @@ const Header = () => {
               />
             </div>
             {/* Cart Icon */}
-            <button aria-label="Cart">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6 text-headerColour hover:text-gray-300"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-                />
-              </svg>
-            </button>
+            <div className="relative">
+              <button aria-label="Cart" onClick={() => navigate('/cart')} className="relative">
+                {cartItemCount > 0 && (
+                  <label className="absolute -top-2 -right-2 w-5 h-5 flex items-center justify-center text-white bg-red-600 rounded-full text-xs font-bold">
+                    {cartItemCount}
+                  </label>
+                )}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 text-headerColour hover:text-gray-300"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+                  />
+                </svg>
+              </button>
+            </div>
+
 
             {/* User Dropdown */}
             <div
@@ -123,15 +152,15 @@ const Header = () => {
               <AiOutlineUser onClick={toggleDropdown} />
               {dropdownOpen && (
                 <div
-                  className={`absolute right-0 mt-2 w-44 bg-navyBlue shadow-lg rounded-lg overflow-hidden transition-transform transform ${
-                    dropdownOpen
-                      ? "scale-100 opacity-100"
-                      : "scale-95 opacity-0"
-                  } duration-200 ease-in-out`}
+                  className={`absolute right-0 mt-2 w-44 bg-navyBlue shadow-lg rounded-lg overflow-hidden transition-transform transform ${dropdownOpen
+                    ? "scale-100 opacity-100"
+                    : "scale-95 opacity-0"
+                    } duration-200 ease-in-out`}
                 >
                   <ul className="py-2">
                     {isLoggedIn ? (
                       <>
+
                         <li className="px-4 py-2 text-sm hover:bg-gray-100 cursor-pointer">
                           <button
                             onClick={() => {
@@ -167,7 +196,7 @@ const Header = () => {
                         </li>
                         <li className="px-4 py-2 text-sm hover:bg-red-100 text-red-600 cursor-pointer">
                           <button
-                            onClick={handleLogout}
+                            onClick={handleLogout} // Logout button now triggers the API call
                             className="w-full text-left"
                           >
                             Sign Out
@@ -175,9 +204,10 @@ const Header = () => {
                         </li>
                       </>
                     ) : (
+                      // Show Login button if user is not logged in
                       <li className="px-4 py-2 text-sm hover:bg-gray-800 cursor-pointer">
                         <button
-                          onClick={handleLogin}
+                          onClick={handleLogin} // Navigates to the login page
                           className="w-full text-left"
                         >
                           Login
