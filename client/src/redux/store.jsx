d4437bd55ec5746ced0215a9b0cf3321a42cf967
Suraj Kumar <kumarsuraj.islampur@gmail.com>
2024-12-05 01:06:50 +0530

    add cartReducer

--- client/src/redux/store.jsx
+++ client/src/redux/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import meDataReducer from "./userSlice";
+import cartReducer from "./cartSlice";
  const store = configureStore({
   reducer: {
     meData: meDataReducer,
+    cart: cartReducer
   },
 });
 
